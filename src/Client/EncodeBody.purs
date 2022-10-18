483121db4e026d3813c4d6fda03197dcf71d509a
filipkrunic <fkrunic9@gmail.com>
2022-10-18 16:27:37 -0700

    encodeBody uses argonaut

--- src/Client/EncodeBody.purs
+++ src/Client/EncodeBody.purs
@@ -1,21 +1,22 @@
 module Payload.Client.EncodeBody where
 
+import Data.Argonaut.Core (stringify)
+import Data.Argonaut.Encode (class EncodeJson, encodeJson)
 import Data.Maybe (Maybe(..))
 import Payload.ContentType (class HasContentType)
-import Simple.JSON as SimpleJson
+import Prelude ((>>>))
 
-class
-  (HasContentType body) <= EncodeBody body where
+class (HasContentType body) <= EncodeBody body where
   encodeBody :: body -> String
 
 instance encodeBodyString :: EncodeBody String where
   encodeBody b = b
 
-instance encodeBodyRecord :: SimpleJson.WriteForeign (Record r) => EncodeBody (Record r) where
-  encodeBody = SimpleJson.writeJSON
+instance encodeBodyRecord :: EncodeJson (Record r) => EncodeBody (Record r) where
+  encodeBody = encodeJson >>> stringify
 
-instance encodeBodyArray :: SimpleJson.WriteForeign (Array r) => EncodeBody (Array r) where
-  encodeBody = SimpleJson.writeJSON
+instance encodeBodyArray :: EncodeJson (Array r) => EncodeBody (Array r) where
+  encodeBody = encodeJson >>> stringify
 
 instance encodeBodyMaybe :: EncodeBody a => EncodeBody (Maybe a) where
   encodeBody Nothing = ""
